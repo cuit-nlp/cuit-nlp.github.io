51b348e3f35a1b5fe010764a0310758ded077638
gaozhengjie <gaozhengj@foxmail.com>
2023-08-12 01:24:50 +0800

    modify config

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -4,8 +4,11 @@ import { defineConfig } from 'vitepress';
 export default defineConfig({
   title: 'Chengdu University of Information Technology',
   description: 'home page of CUIT-NLP group',
+  base: '/',
+  // 主题配置
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
+    // 头部导航
     nav: [
       { text: 'Home', link: '/' },
       { text: 'Research', link: '/research' },
@@ -13,9 +16,5 @@ export default defineConfig({
       { text: 'Publications', link: '/publications' },
       { text: 'Contact', link: '/contact' },
     ],
-
-    // socialLinks: [
-    //   { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
-    // ]
   },
 });
